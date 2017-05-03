18b3457dad2477a36cb4dd71279fa4fa621e1dcd
mohamedbayoudh <mohamed-bayoudh@outlook.de>
2017-05-03 11:30:22 +0200

    e

---
+++ src/main/java/edu/kit/ipd/swt1/EditMe.java
@@ -0,0 +1,27 @@
+package edu.kit.ipd.swt1;
+
+
+/**
+ * 
+ * @author mohamed
+ * class EditMe
+ */
+public class EditMe {
+
+	/**
+	 * 
+	 * @return "bare"
+	 */
+	public String getFoo() {
+		return "bar";
+	}
+	
+	/**
+	 * 
+	 * @return your matriculation number
+	 */
+	public int getMatNum() {
+		//TODO implement me
+		return 2082466;
+	}
+}
